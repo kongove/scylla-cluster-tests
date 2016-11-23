a048569d7b40d852f5c930d6e9dadd29ce507a67
Amos Kong <amos@scylladb.com>
2016-11-24 06:04:17 +0800

    output c-s status every 60s
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- performance_regression_test.py
+++ performance_regression_test.py
@@ -135,7 +135,7 @@ class PerformanceRegressionTest(ClusterTester):
         # run a write workload
         base_cmd = ("cassandra-stress %s no-warmup cl=QUORUM duration=1m "
                     "-schema 'replication(factor=3)' -port jmx=6868 "
-                    "-log interval=3600 "
+                    "-log interval=60 "
                     "-mode cql3 native -rate threads=500 -errors ignore "
                     "-pop seq=1..100000000")
 
@@ -164,7 +164,7 @@ class PerformanceRegressionTest(ClusterTester):
         # run a write workload
         base_cmd = ("cassandra-stress write no-warmup cl=QUORUM duration=60m "
                     "-schema 'replication(factor=3)' -port jmx=6868 "
-                    "-log interval=3600 "
+                    "-log interval=60 "
                     "-mode cql3 native -rate threads=1000 -errors ignore "
                     "-pop seq=1..10000000")
 
@@ -185,12 +185,12 @@ class PerformanceRegressionTest(ClusterTester):
         """
         base_cmd_w = ("cassandra-stress write cl=QUORUM n=30000000 "
                       "-schema 'replication(factor=3)' -port jmx=6868 "
-                      "-log interval=3600 "
+                      "-log interval=60 "
                       "-mode cql3 native -rate threads=500 -errors ignore "
                       "-pop seq=1..30000000")
         base_cmd_r = ("cassandra-stress read cl=QUORUM duration=50m "
                       "-schema 'replication(factor=3)' -port jmx=6868 "
-                      "-log interval=3600 "
+                      "-log interval=60 "
                       "-mode cql3 native -rate threads=500 -errors ignore "
                       "-pop 'dist=gauss(1..30000000,15000000,1500000)' ")
 
@@ -216,17 +216,17 @@ class PerformanceRegressionTest(ClusterTester):
         """
         base_cmd_w = ("cassandra-stress write no-warmup cl=QUORUM n=30000000 "
                       "-schema 'replication(factor=3)' -port jmx=6868 "
-                      "-log interval=3600 "
+                      "-log interval=60 "
                       "-mode cql3 native -rate threads=500 -errors ignore "
                       "-pop seq=1..30000000")
         base_cmd_r = ("cassandra-stress read no-warmup cl=QUORUM duration=50m "
                       "-schema 'replication(factor=3)' -port jmx=6868 "
-                      "-log interval=3600 "
+                      "-log interval=60 "
                       "-mode cql3 native -rate threads=500 -errors ignore "
                       "-pop 'dist=gauss(1..30000000,15000000,1500000)' ")
         base_cmd_m = ("cassandra-stress mixed no-warmup cl=QUORUM duration=50m "
                       "-schema 'replication(factor=3)' -port jmx=6868 "
-                      "-log interval=3600 "
+                      "-log interval=60 "
                       "-mode cql3 native -rate threads=500 -errors ignore "
                       "-pop 'dist=gauss(1..30000000,15000000,1500000)' ")
 
