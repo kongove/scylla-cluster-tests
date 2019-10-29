b9d8e10d93419778ad56bbb1473fc6f9fb794e82
Amos Kong <amos@scylladb.com>
2019-10-30 04:24:55 +0800

    fill_db_data: create tables together then truncate them
    I touched a schema disagreement issue in fill_db_data testing during upgrade,
    we want to sleep 30s after creating test tables. For saving the test time,
    let's create test tables together, and only sleep once, then truncate tables
    one by one.
    
    Signed-off-by: Amos Kong <amos@scylladb.com>
    (cherry picked from commit 2255c0e5ebb876f6f4295a2a9c53ccbb672d7bdc)
    
    Conflicts:
    	sdcm/fill_db_data.py

--- sdcm/fill_db_data.py
+++ sdcm/fill_db_data.py
@@ -17,6 +17,7 @@ __author__ = 'Roy Dahan'
 
 
 import random
+import time
 from collections import OrderedDict
 from uuid import UUID
 
@@ -2871,13 +2872,19 @@ class FillDatabaseData(ClusterTester):
             self.cql_insert_data_to_simple_tables(session, rows=insert_rows)
 
     def cql_create_tables(self, session):
+        truncates = []
         # Run through the list of items and create all tables
         for a in self.all_verification_items:
             if not a['skip'] and ('skip_condition' not in a or eval(str(a['skip_condition']))):
                 for create_table in a['create_tables']:
                     session.execute(create_table)
-                for truncate in a['truncates']:
-                    session.execute(truncate)
+                for truncate in item['truncates']:
+                    truncates.append(truncate)
+        # Sleep a while after creating test tables to avoid schema disagreement.
+        # Refs: https://github.com/scylladb/scylla/issues/5235
+        time.sleep(30)
+        for truncate in truncates:
+            session.execute(truncate)
 
     def cql_insert_data_to_tables(self, session, default_fetch_size):
         for a in self.all_verification_items:
