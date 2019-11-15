47055e518dcda754fbc52033e06842b151744377
Amos Kong <amos@scylladb.com>
2019-11-16 00:28:50 +0800

    update
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- upgrade_test.py
+++ upgrade_test.py
@@ -174,7 +174,7 @@ class UpgradeTest(FillDatabaseData):
                     node.remoter.run('sudo apt-get dist-upgrade {} -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes --allow-unauthenticated'.format(scylla_pkg))
         if self.params.get('test_sst3', default=None):
             node.remoter.run("echo 'enable_sstables_mc_format: true' |sudo tee --append /etc/scylla/scylla.yaml")
-        if self.params.get('test_upgrade_3_1_0', default=True):
+        if self.params.get('test_upgrade_3_1_0', default=False):
             node.remoter.run("echo 'enable_3_1_0_compatibility_mode: true' |sudo tee --append /etc/scylla/scylla.yaml")
         authorization_in_upgrade = self.params.get('authorization_in_upgrade', default=None)
         if authorization_in_upgrade:
@@ -250,7 +250,7 @@ class UpgradeTest(FillDatabaseData):
             node.remoter.run('bash /tmp/recover_system_tables.sh %s' % snapshot_name[0], verbose=True)
         if self.params.get('test_sst3', default=None):
             node.remoter.run('sudo sed -i -e "s/enable_sstables_mc_format:/#enable_sstables_mc_format:/g" /etc/scylla/scylla.yaml')
-        if self.params.get('test_upgrade_3_1_0', default=True):
+        if self.params.get('test_upgrade_3_1_0', default=False):
             node.remoter.run('sudo sed -i -e "s/enable_3_1_0_compatibility_mode:/#enable_3_1_0_compatibility_mode:/g" /etc/scylla/scylla.yaml')
         if self.params.get('remove_authorization_in_rollback', default=None):
             node.remoter.run('sudo sed -i -e "s/authorizer:/#authorizer:/g" /etc/scylla/scylla.yaml')
